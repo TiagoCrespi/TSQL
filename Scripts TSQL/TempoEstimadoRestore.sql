3c2bcff8043c0c1223ef3441276df892310a1627
TiagoCrespi <tiago.crespi@outlook.com.br>
2026-01-08 13:36:04 -0300

    Adicionada nova query que retorna o tempo estimado de uma consulta.

---
+++ Scripts TSQL/TempoEstimadoRestore.sql
@@ -0,0 +1,9 @@
+SELECT command,
+               'EstimatedEndTime' = Dateadd(ms,estimated_completion_time,Getdate()),
+               'EstimatedSecondsToEnd' = estimated_completion_time / 1000,
+               'EstimatedMinutesToEnd' = estimated_completion_time / 1000 / 60,
+               'BackupStartTime' = start_time,
+               'TimeExecution' =  DateDiff(minute,Getdate(),Start_time),
+               'PercentComplete' = percent_complete
+FROM sys.dm_exec_requests
+WHERE command = 'RESTORE LOG'
